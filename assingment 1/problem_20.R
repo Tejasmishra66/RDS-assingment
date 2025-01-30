5c3b04d73e71ac9fecff2c62cc3def345b539738
Tejasmishra66 <06tejasmishra06@gmail.com>
2025-01-30 20:58:44 +0530

    Create problem_20.R

---
+++ assingment 1/problem_20.R
@@ -0,0 +1,20 @@
+
+sequence <- seq(from=15, to=25, by=1)
+sequence
+
+repeat_vector <- rep(4.2, times=2)
+repeat_vector
+
+y <- -5
+y
+
+a <- seq(from=5, to=-11, by=-0.3)
+ a
+
+rev(a)
+length(rev(a))
+
+sequence <- seq(from=200, to=length(rev(a)),length.out=12)
+sequence
+ #Output [1] 200.00000 186.72727 173.45455 160.18182 146.90909 133.63636 120.36364 107.09091  93.81818  80.54545  67.27273
+       # [12]  54.00000
