77654591dad5335f7082c5a66aa9da6ab0266e0d
Tejasmishra66 <06tejasmishra06@gmail.com>
2025-01-30 21:19:59 +0530

    Update problem_26.R

--- assingment 1/problem_26.R
+++ assingment 1/problem_26.R
@@ -1,1 +1,10 @@
 
+fahrenheit_temps <- c(32, 68, 14, 0, 113, 104, 198)
+
+# Convert to Celsius using vectorized arithmetic
+celsius_temps <- (5/9) * (fahrenheit_temps - 32)
+
+# Print the results
+print(celsius_temps)
+
+output [1]  0.00000 20.00000 -10.00000 -17.77778 45.00000 40.00000 92.22222
