671ddfc19f009b2475dccadfa9e9500cd234a0be
Tejasmishra66 <06tejasmishra06@gmail.com>
2025-01-30 21:25:18 +0530

    Update problem_25.R

--- assingment 1/problem_25.R
+++ assingment 1/problem_25.R
@@ -1,1 +1,10 @@
 
+v <- c(3,1,2,3,1,2,3,1,2)
+
+rep_vec <- c(2, 2)  # This ensures that we can replace all elements with 2s
+
+result <- rep_vec[v %% 2 + 1] 
+
+print(result)
+
+output [1] 2 2 2 2 2 2 2 2 2
