e1c735c3eb35f26739b431dbbe34147ec9702107
Tejasmishra66 <06tejasmishra06@gmail.com>
2025-01-30 20:51:32 +0530

    Create problem_14.R

---
+++ assingment 1/problem_14.R
@@ -0,0 +1,6 @@
+# Convert the vector c(2,0.5,1,2,0.5,1,2,0.5,1) to a vector of only 1s, using a vector of length 3.
+original_vector <- c(2, 0.5, 1, 2, 0.5, 1, 2, 0.5, 1)
+vector_of_ones <- rep(1, length(original_vector))
+print(vector_of_ones)
+
+# Output  [1] 1 1 1 1 1 1 1 1 1
