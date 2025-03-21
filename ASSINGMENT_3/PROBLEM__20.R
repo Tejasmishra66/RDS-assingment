40b81e2f84750422ba5673281bf3bc6e8a8c9c14
Tejasmishra66 <06tejasmishra06@gmail.com>
2025-03-21 21:48:20 +0530

    Create PROBLEM__20.R

---
+++ ASSINGMENT_3/PROBLEM__20.R
@@ -0,0 +1,11 @@
+#Write an R script that creates a string containing a newline, tab, and a backslash using escape sequences. Print the 
+#string to the console using cat().
+
+
+
+str <- "Hello,\n\tThis is a backslash: \\"
+cat(str)
+
+#OUTPUT:
+Hello,
+	This is a backslash: \
